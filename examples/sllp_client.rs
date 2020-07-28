8c6a476b1351c1d65d3dda482828e503332be341
Julian Lazaras <umbrasdracodominus@gmail.com>
2020-07-27 21:27:50 -0700

    modified sllp_client example to add set_socket_addr fn

--- examples/sllp_client.rs
+++ examples/sllp_client.rs
@@ -1,11 +1,13 @@
 use networking::sllp::SllpSocket;
+use networking::Layer3Addr;
 use networking::test_config;
 use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let (peer, config) = test_config();
+    let (mut peer, config) = test_config();
     let mut socket = SllpSocket::from_host_data(&config).await?;
+    peer.set_socket_addr((Layer3Addr::newv4(127,0,0,1), 6464).into());
     let mut stream = socket.connect(&peer).await;
     Ok(())
 }
