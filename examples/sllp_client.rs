8d986adfb5c47372b9f46db8eedaea1c6debe707
Julian Lazaras <umbrasdracodominus@gmail.com>
2020-07-28 06:14:15 -0700

    testing sllp examples

--- examples/sllp_client.rs
+++ examples/sllp_client.rs
@@ -2,11 +2,12 @@ use networking::sllp::SllpSocket;
 use networking::test_config;
 use networking::Layer3Addr;
 use std::error::Error;
+use networking::asyncronous::AsyncSend;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let (mut peer, config) = test_config();
-    let mut socket = SllpSocket::from_host_data(&config).await?;
+    let socket = SllpSocket::from_host_data(&config).await?;
     peer.set_socket_addr((Layer3Addr::newv4(127, 0, 0, 1), 6464).into());
     let mut stream = socket.connect(&peer).await;
     stream.send(b"hello world").await.unwrap();
--- examples/sllp_server.rs
+++ examples/sllp_server.rs
@@ -1,17 +1,18 @@
 use networking::sllp::SllpSocket;
 use networking::test_config;
 use std::error::Error;
+use networking::asyncronous::AsyncRecv;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let (peer, config) = test_config();
     let mut socket = SllpSocket::from_host_data(&config).await?;
     while let Some(strm) = socket.incoming().await {
-        let mut stream = unsafe { strm.unwrap().unverify() };
+        let mut stream = strm?.verify(&peer)?;
         tokio::spawn(async move {
             loop {
                 let mut invec = Vec::new();
-                stream.recv(&mut invec).await;
+                stream.recv(&mut invec).await.unwrap();
                 println!(
                     "got message {}, from server",
                     String::from_utf8(invec).unwrap()
